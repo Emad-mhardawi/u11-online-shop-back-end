6a490f7f56843dd8924e59f040b6e536ed0bb8c7
Emad Mhardawi <emad.mhardawi@gmail.com>
2021-03-02 20:19:09 +0100

    connect to mongoDB data base

---
+++ config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDatabase = async ()=>{
+    try{
+        const connect = await mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+            useUnifiedTopology:true,
+            useNewUrlParser:true,
+            useCreateIndex:true
+        })
+
+        console.log('connected  ' +connect.connection.host)
+    }
+    catch(error){
+        console.log('[error]: ' + error.message);
+        process.exit(1);
+    }
+}
+
+
+export default connectDatabase;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './config/db.js';
+dotenv.config();
+connectDb()
 
 const app = express();
 
 app.use('/' ,(req, res,next)=>{
     res.send('wooorking')
 })
+
 
 app.listen(5000)
